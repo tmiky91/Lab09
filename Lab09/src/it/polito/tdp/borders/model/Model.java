ff76cdeb697ff0636e5c766e73e8db7ab4f9dacf
pc-michi <pc-michi@172.22.16.153>
2019-04-12 15:50:59 +0200

    Bozza soluzione 2

--- Lab09/src/it/polito/tdp/borders/model/Model.java
+++ Lab09/src/it/polito/tdp/borders/model/Model.java
@@ -1,5 +1,7 @@
 package it.polito.tdp.borders.model;
 
+import it.polito.tdp.borders.db.BordersDAO;
+
 public class Model {
 
 	public Model() {
@@ -7,16 +9,19 @@ public class Model {
 	}
 
 	public boolean annoValido(String anno) {
-		int annoValido= Integer.parseInt(anno);
-		if(anno.matches("\\d{4}")) {
-			return true;
+		if(!anno.matches("\\d{4}")) {
+			return false;
 		}
-		return false;
+		int annoValido= Integer.parseInt(anno);
+		return annoValido<=1816 && annoValido>=2016;
 	}
 
 	public String calcolaConfini(String anno) {
+		String risultato="";
+		BordersDAO dao = new BordersDAO();
 		
-		return null;
+		
+		return risultato;
 	}
 
 }
